4a1bc9514bf62aa783eb0c14d95defe4f9e1cd89
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-20 19:48:28 +0700

    feat(project): setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="underline text-red-600">Hello World</div>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import Unocss from "unocss/vite";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), Unocss()],
+});
