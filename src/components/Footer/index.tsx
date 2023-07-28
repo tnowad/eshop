c08fb062073f5afe7b1526a4514415278b3b5f8b
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-28 15:40:24 +0700

    feat(footer): add footer components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,16 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="py-3 flex items-center justify-center">
+      <div className="container flex justify-center">
+        <a
+          href="https://github.com/tnowad"
+          className="font-extralight hover:text-red-5 duration-300"
+        >
+          Nguyen Minh Tuan
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
