a12d34215718ead2ea50300fb83e57a47b0aa43a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 05:00:35 +0700

    feat(signup): add signup components

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,93 @@
+import { Link } from "react-router-dom";
+import Helmet from "../components/Helmet";
+import { useState } from "react";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState<{
+    username: string;
+    email: string;
+    password: string;
+    file: object | null;
+  }>({ username: "", email: "", password: "", file: null });
+  const handleSubmit = () => {
+    console.log(typeof formData.file);
+  };
+
+  return (
+    <Helmet title="Login">
+      <section className="flex justify-center">
+        <div className="container flex justify-center ">
+          <div className="w-[min(100%,_500px)]">
+            <h3 className="text-center text-3xl font-bold">Sign Up</h3>
+            <div>
+              <div className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3">
+                <input
+                  type="username"
+                  value={formData.username}
+                  onChange={(event) => {
+                    setFormData((value) => ({
+                      ...value,
+                      username: event.target.value,
+                    }));
+                  }}
+                  placeholder="Enter your username..."
+                  className="outline-none w-full"
+                />
+              </div>
+              <div className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3">
+                <input
+                  type="email"
+                  value={formData.email}
+                  onChange={(event) => {
+                    setFormData((value) => ({
+                      ...value,
+                      email: event.target.value,
+                    }));
+                  }}
+                  placeholder="Enter your email..."
+                  className="outline-none w-full"
+                />
+              </div>
+              <div className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3">
+                <input
+                  type="password"
+                  value={formData.password}
+                  onChange={(event) => {
+                    setFormData((value) => ({
+                      ...value,
+                      password: event.target.value,
+                    }));
+                  }}
+                  placeholder="Enter your password..."
+                  className="outline-none w-full"
+                />
+              </div>
+              <div className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3">
+                <input
+                  type="file"
+                  onChange={(event) =>
+                    setFormData((value) => ({
+                      ...value,
+                      file: event.target.files?.item(0) as object,
+                    }))
+                  }
+                />
+              </div>
+              <button
+                className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3 justify-center"
+                onClick={handleSubmit}
+              >
+                Sign Up
+              </button>
+              <p>
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Helmet>
+  );
 };
 
 export default SignUp;
