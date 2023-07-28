2d4040dd9c8908383c75616ee9a1bc03542c6d5a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 00:32:23 +0700

    refactor(products): move products to new mock file

---
+++ src/mocks/products.ts
@@ -0,0 +1,119 @@
+import Product from "../interfaces/Product";
+import chair from "../assets/chair.png";
+
+const products: Product[] = [
+  {
+    id: "2",
+    category: "chair",
+    imageUrl: chair,
+    price: 150,
+    productName: "Fake Chair 1",
+  },
+  {
+    id: "3",
+    category: "chair",
+    imageUrl: chair,
+    price: 180,
+    productName: "Fake Chair 2",
+  },
+  {
+    id: "4",
+    category: "chair",
+    imageUrl: chair,
+    price: 220,
+    productName: "Fake Chair 3",
+  },
+  {
+    id: "5",
+    category: "chair",
+    imageUrl: chair,
+    price: 80,
+    productName: "Fake Chair 4",
+  },
+  {
+    id: "6",
+    category: "chair",
+    imageUrl: chair,
+    price: 120,
+    productName: "Fake Chair 5",
+  },
+  {
+    id: "7",
+    category: "chair",
+    imageUrl: chair,
+    price: 190,
+    productName: "Fake Chair 6",
+  },
+  {
+    id: "8",
+    category: "chair",
+    imageUrl: chair,
+    price: 160,
+    productName: "Fake Chair 7",
+  },
+  {
+    id: "9",
+    category: "chair",
+    imageUrl: chair,
+    price: 250,
+    productName: "Fake Chair 8",
+  },
+  {
+    id: "10",
+    category: "sofa",
+    imageUrl: chair,
+    price: 350,
+    productName: "Fake Sofa 1",
+  },
+  {
+    id: "11",
+    category: "sofa",
+    imageUrl: chair,
+    price: 280,
+    productName: "Fake Sofa 2",
+  },
+  {
+    id: "12",
+    category: "sofa",
+    imageUrl: chair,
+    price: 400,
+    productName: "Fake Sofa 3",
+  },
+  {
+    id: "13",
+    category: "sofa",
+    imageUrl: chair,
+    price: 500,
+    productName: "Fake Sofa 4",
+  },
+  {
+    id: "14",
+    category: "sofa",
+    imageUrl: chair,
+    price: 420,
+    productName: "Fake Sofa 5",
+  },
+  {
+    id: "15",
+    category: "sofa",
+    imageUrl: chair,
+    price: 380,
+    productName: "Fake Sofa 6",
+  },
+  {
+    id: "16",
+    category: "sofa",
+    imageUrl: chair,
+    price: 320,
+    productName: "Fake Sofa 7",
+  },
+  {
+    id: "17",
+    category: "sofa",
+    imageUrl: chair,
+    price: 450,
+    productName: "Fake Sofa 8",
+  },
+];
+
+export default products;
