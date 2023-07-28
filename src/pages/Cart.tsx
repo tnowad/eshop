ee2be94931e2621fa6216f133e0fc2121df4bebc
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 03:51:37 +0700

    feat(cart): add show text no item when cart dont have item

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -16,59 +16,65 @@ const Cart = () => {
       <section className="flex justify-center">
         <div className="container">
           <div className="grid lg:grid-cols-[80%_20%]">
-            <table className="table w-full mt-3">
-              <thead>
-                <tr className="border-b">
-                  <th>Image</th>
-                  <th>Title</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item) => (
-                  <tr
-                    key={item.id}
-                    className="hover:shadow-sm hover:bg-slate-1/20 border-b"
-                  >
-                    <th className="flex justify-center">
-                      <img
-                        src={item.imageUrl}
-                        alt={item.productName}
-                        className="w-20"
-                      />
-                    </th>
-                    <th>{item.productName}</th>
-                    <th>${item.price}</th>
-                    <th>
-                      <input
-                        type="number"
-                        value={item.quantity}
-                        onChange={(event) => {
-                          dispatch(
-                            updateCartItem({
-                              item,
-                              quantity: event.target.value,
-                            })
-                          );
-                        }}
-                      />
-                    </th>
-                    <th>
-                      <button
-                        onClick={() => {
-                          dispatch(removeCartItem(item.id));
-                          toast.error("Removed product in cart!");
-                        }}
-                      >
-                        <Icon icon="material-symbols:delete-outline" />
-                      </button>
-                    </th>
+            {cartItems.length > 0 ? (
+              <table className="table w-full mt-3">
+                <thead>
+                  <tr className="border-b">
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Delete</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr
+                      key={item.id}
+                      className="hover:shadow-sm hover:bg-slate-1/20 border-b"
+                    >
+                      <th className="flex justify-center">
+                        <img
+                          src={item.imageUrl}
+                          alt={item.productName}
+                          className="w-20"
+                        />
+                      </th>
+                      <th>{item.productName}</th>
+                      <th>${item.price}</th>
+                      <th>
+                        <input
+                          type="number"
+                          value={item.quantity}
+                          onChange={(event) => {
+                            dispatch(
+                              updateCartItem({
+                                item,
+                                quantity: event.target.value,
+                              })
+                            );
+                          }}
+                        />
+                      </th>
+                      <th>
+                        <button
+                          onClick={() => {
+                            dispatch(removeCartItem(item.id));
+                            toast.error("Removed product in cart!");
+                          }}
+                        >
+                          <Icon icon="material-symbols:delete-outline" />
+                        </button>
+                      </th>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <h1 className="text-2xl font-bold text-center mt-15">
+                No item added to the cart
+              </h1>
+            )}
             <div className="mt-2 lg:px-4">
               <h2 className="text-xl font-bold">Subtotal</h2>
               <span>${totalAmount}</span>
