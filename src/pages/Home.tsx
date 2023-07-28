045fee6a3601553865d3b7f14fa84b2880b440a1
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-28 15:33:17 +0700

    feat(home): add render trending and best sales products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,10 @@ import Helmet from "../components/Helmet";
 import banner from "../assets/banner.webp";
 import { Link } from "react-router-dom";
 import Service from "../components/Service";
+import ProductList from "../components/UI/ProductList";
+import { useEffect, useState } from "react";
+import Product from "../interfaces/Product";
+import chair from "../assets/chair.png";
 
 const services = [
   {
@@ -26,7 +30,135 @@ const services = [
   },
 ];
 
+const products: Product[] = [
+  {
+    id: "2",
+    category: "chair",
+    imageUrl: chair,
+    price: 150,
+    productName: "Fake Chair 1",
+  },
+  {
+    id: "3",
+    category: "chair",
+    imageUrl: chair,
+    price: 180,
+    productName: "Fake Chair 2",
+  },
+  {
+    id: "4",
+    category: "chair",
+    imageUrl: chair,
+    price: 220,
+    productName: "Fake Chair 3",
+  },
+  {
+    id: "5",
+    category: "chair",
+    imageUrl: chair,
+    price: 80,
+    productName: "Fake Chair 4",
+  },
+  {
+    id: "6",
+    category: "chair",
+    imageUrl: chair,
+    price: 120,
+    productName: "Fake Chair 5",
+  },
+  {
+    id: "7",
+    category: "chair",
+    imageUrl: chair,
+    price: 190,
+    productName: "Fake Chair 6",
+  },
+  {
+    id: "8",
+    category: "chair",
+    imageUrl: chair,
+    price: 160,
+    productName: "Fake Chair 7",
+  },
+  {
+    id: "9",
+    category: "chair",
+    imageUrl: chair,
+    price: 250,
+    productName: "Fake Chair 8",
+  },
+  {
+    id: "10",
+    category: "sofa",
+    imageUrl: chair,
+    price: 350,
+    productName: "Fake Sofa 1",
+  },
+  {
+    id: "11",
+    category: "sofa",
+    imageUrl: chair,
+    price: 280,
+    productName: "Fake Sofa 2",
+  },
+  {
+    id: "12",
+    category: "sofa",
+    imageUrl: chair,
+    price: 400,
+    productName: "Fake Sofa 3",
+  },
+  {
+    id: "13",
+    category: "sofa",
+    imageUrl: chair,
+    price: 500,
+    productName: "Fake Sofa 4",
+  },
+  {
+    id: "14",
+    category: "sofa",
+    imageUrl: chair,
+    price: 420,
+    productName: "Fake Sofa 5",
+  },
+  {
+    id: "15",
+    category: "sofa",
+    imageUrl: chair,
+    price: 380,
+    productName: "Fake Sofa 6",
+  },
+  {
+    id: "16",
+    category: "sofa",
+    imageUrl: chair,
+    price: 320,
+    productName: "Fake Sofa 7",
+  },
+  {
+    id: "17",
+    category: "sofa",
+    imageUrl: chair,
+    price: 450,
+    productName: "Fake Sofa 8",
+  },
+];
+
 const Home = () => {
+  const [trendingProducts, setTrendingProduct] = useState<Product[]>([]);
+  const [bestSalesProducts, setBestSalesProduct] = useState<Product[]>([]);
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+    setTrendingProduct(filteredTrendingProducts);
+    const filteredBestSalesProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+    setBestSalesProduct(filteredBestSalesProducts);
+  });
+
   return (
     <Helmet title="Home">
       <section>
@@ -57,6 +189,22 @@ const Home = () => {
           {services.map((item) => {
             return <Service {...item} />;
           })}
+        </div>
+      </section>
+      <section className="flex justify-center pt-4">
+        <div className="container">
+          <h2 className="font-bold text-3xl text-center py-10">
+            Trending Products
+          </h2>
+          <ProductList products={trendingProducts} />
+        </div>
+      </section>
+      <section className="flex justify-center pt-4">
+        <div className="container">
+          <h2 className="font-bold text-3xl text-center py-10">
+            Best Sales Products
+          </h2>
+          <ProductList products={bestSalesProducts} />
         </div>
       </section>
     </Helmet>
