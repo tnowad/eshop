cf13f8d9d1b17328d7c4ea4612740da45ebd5a79
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 05:35:10 +0700

    feat(firebase): setup firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXcTPFth9I4DypLkH60UrH8H2_ina8WHM",
+  authDomain: "eshop-b0351.firebaseapp.com",
+  projectId: "eshop-b0351",
+  storageBucket: "eshop-b0351.appspot.com",
+  messagingSenderId: "345520183148",
+  appId: "1:345520183148:web:179c56dc4f7d237523c549",
+  measurementId: "G-7YFC5XWB0P",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
