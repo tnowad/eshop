9abaeea3d96e321aa526221b37db7699c6ffa5b6
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 06:13:06 +0700

    feat(signup): add signup with firebase

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,16 +1,56 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Helmet from "../components/Helmet";
 import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { toast } from "react-toastify";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const SignUp = () => {
   const [formData, setFormData] = useState<{
     username: string;
     email: string;
     password: string;
-    file: object | null;
+    file: any | null;
   }>({ username: "", email: "", password: "", file: null });
-  const handleSubmit = () => {
-    console.log(typeof formData.file);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async () => {
+    setLoading(false);
+    const { username, email, password } = formData;
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const storageRef = ref(storage, `images/${Date.now() + username}`);
+      const uploadTask = uploadBytesResumable(storageRef, formData.file);
+      uploadTask.on("state_changed", () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadUrl) => {
+          await updateProfile(user, {
+            displayName: username,
+            photoURL: downloadUrl,
+          });
+
+          await setDoc(doc(db, "users", user.uid), {
+            uid: user.uid,
+            displayName: user.displayName,
+            email,
+            photoURL: downloadUrl,
+          });
+        });
+      });
+      const { user } = userCredential;
+      toast.success("Account created");
+      navigate("/login");
+      setLoading(false);
+    } catch (error) {
+      toast.error("Something went wrong!");
+      setLoading(false);
+    }
   };
 
   return (
@@ -76,6 +116,7 @@ const SignUp = () => {
               <button
                 className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full mt-3 justify-center"
                 onClick={handleSubmit}
+                disabled={loading}
               >
                 Sign Up
               </button>
