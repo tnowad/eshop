cde3d30afc7914eedf2cf781b160357eaa5fb2c2
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 00:32:54 +0700

    feat(shop): add function search products

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,10 +1,110 @@
+import { Icon } from "@iconify/react";
 import CommonSection from "../components/CommonSection";
+import Helmet from "../components/Helmet";
+import { useEffect, useState } from "react";
+import Product from "../interfaces/Product";
+import ProductList from "../components/UI/ProductList";
+import products from "../mocks/products";
 
 const Shop = () => {
+  const [productsData, setProductsData] = useState<Product[]>(products);
+
+  const [searchForm, setSearchForm] = useState<{
+    category: string;
+    sortBy: string;
+    keyword: string;
+  }>({ category: "", sortBy: "", keyword: "" });
+
+  useEffect(() => {
+    const filteredProducts = products.filter((item) => {
+      let flag = true;
+      if (searchForm.category !== "" && item.category !== searchForm.category) {
+        flag = false;
+      }
+      if (
+        searchForm.keyword !== "" &&
+        !item.productName.includes(searchForm.keyword)
+      ) {
+        flag = false;
+      }
+      return flag;
+    });
+
+    if (searchForm.sortBy !== "") {
+      if (searchForm.sortBy === "asc") {
+        filteredProducts.sort((a, b) =>
+          a.productName.localeCompare(b.productName)
+        );
+      } else if (searchForm.sortBy === "des") {
+        filteredProducts.sort(
+          (a, b) => a.productName.localeCompare(b.productName) * -1
+        );
+      }
+    }
+
+    setProductsData(filteredProducts);
+  }, [searchForm]);
+
   return (
-    <div>
+    <Helmet title="Shop">
       <CommonSection title="Products" />
-    </div>
+      <section className="flex justify-center pt-4">
+        <div className="container">
+          <div className="flex justify-between flex-wrap lg:flex-nowrap gap-y-2">
+            <select
+              className="p-3 shadow-md outline-none bg-white rounded w-full lg:w-30%"
+              onChange={(event) =>
+                setSearchForm((value) => ({
+                  ...value,
+                  category: event.target.value,
+                }))
+              }
+            >
+              <option value="">Filter by Category</option>
+              <option value="sofa">Sofa</option>
+              <option value="chair">Chair</option>
+              <option value="phone">Phone</option>
+            </select>
+            <select
+              className="p-3 shadow-md outline-none bg-white rounded w-full lg:w-30%"
+              onChange={(event) =>
+                setSearchForm((value) => ({
+                  ...value,
+                  sortBy: event.target.value,
+                }))
+              }
+            >
+              <option value="">Sort By</option>
+              <option value="asc">Ascending</option>
+              <option value="des">Descending</option>
+            </select>
+            <div className="flex items-center p-3 shadow-md outline-none bg-white rounded w-full lg:w-30%">
+              <input
+                type="text"
+                placeholder="Search....."
+                className="outline-none w-full"
+                onChange={(event) =>
+                  setSearchForm((value) => ({
+                    ...value,
+                    keyword: event.target.value,
+                  }))
+                }
+              />
+              <Icon icon="material-symbols:search" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex justify-center pt-4">
+        <div className="container">
+          {productsData.length > 0 ? (
+            <ProductList products={productsData} />
+          ) : (
+            <h1>No products are found!</h1>
+          )}
+        </div>
+      </section>
+    </Helmet>
   );
 };
 
