8addac4108557c33e43a34a2aa3595b64e54e962
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 02:09:30 +0700

    feat(product): update product attribute

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,7 +1,12 @@
+import Review from "./Review";
+
 export default interface Product {
   id: string;
   imageUrl: string;
   productName: string;
   category: string;
   price: number;
+  avgRating: number;
+  reviews: Review[];
+  description: string;
 }
