e5956c0443aee5ca18d5b57b4495230424fc7012
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 12:30:32 +0700

    feat(header): add check location to show header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,18 @@
+import { useLocation } from "react-router-dom";
 import Router from "../../routers";
 import Footer from "../Footer";
 import Header from "../Header";
+import AdminHeader from "../Header/AdminHeader";
 
 const Layout = () => {
+  const location = useLocation();
   return (
     <>
-      <Header />
+      {location.pathname.startsWith("/dashboard") ? (
+        <AdminHeader />
+      ) : (
+        <Header />
+      )}
       <div>
         <Router />
       </div>
