9fa35cab6838c85e05437f7bc858c6d18543a4da
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 12:31:22 +0700

    feat(routers): add path dashboard

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -8,6 +8,11 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import NotFound from "../pages/NotFound";
 import ProtectedRoute from "./ProtectedRoute";
+import AllProducts from "../pages/admin/AllProducts";
+import AddProducts from "../pages/admin/AddProducts";
+import Orders from "../pages/admin/Orders";
+import Users from "../pages/admin/Users";
+import Dashboard from "../pages/admin/Dashboard";
 
 const Router = () => {
   return (
@@ -21,6 +26,13 @@ const Router = () => {
       <Route element={<ProtectedRoute />}>
         <Route path="checkout" element={<Checkout />} />
         <Route path="cart" element={<Cart />} />
+
+        <Route path="/dashboard" element={<Navigate to="/dashboard/home" />} />
+        <Route path="/dashboard/home" element={<Dashboard />} />
+        <Route path="/dashboard/all-products" element={<AllProducts />} />
+        <Route path="/dashboard/add-products" element={<AddProducts />} />
+        <Route path="/dashboard/orders" element={<Orders />} />
+        <Route path="/dashboard/users" element={<Users />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
