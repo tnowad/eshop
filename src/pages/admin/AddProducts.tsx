ac4b2186452d6d2167cc3b8480bf0a1fb6ad138a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-29 10:23:16 +0700

    feat(product): add components add product

---
+++ src/pages/admin/AddProducts.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import Helmet from "../../components/Helmet";
+import Product from "../../interfaces/Product";
+
+const AddProducts = () => {
+  const initialProductState: Partial<Product> = {
+    productName: "",
+    category: "",
+    price: 0,
+    description: "",
+  };
+
+  const [file, setFile] = useState<File | null>(null);
+
+  const [product, setProduct] = useState<Partial<Product>>(initialProductState);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Product data:", product, "Image file:", file);
+
+    setProduct(initialProductState);
+    setFile(null);
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    if (files?.length) {
+      const selectedFile = files[0];
+      setFile(selectedFile);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProduct({ ...product, imageUrl: reader.result as string });
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  return (
+    <Helmet title="Add Products">
+      <section className="flex justify-center mt-3">
+        <div className="container">
+          <div>
+            <h4 className="text-xl font-bold mb-4">Add Product</h4>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-bold mb-2"
+                  htmlFor="productName"
+                >
+                  Product Name
+                </label>
+                <input
+                  className="w-full p-2 border rounded-md"
+                  type="text"
+                  id="productName"
+                  name="productName"
+                  value={product.productName}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-bold mb-2"
+                  htmlFor="category"
+                >
+                  Category
+                </label>
+                <input
+                  className="w-full p-2 border rounded-md"
+                  type="text"
+                  id="category"
+                  name="category"
+                  value={product.category}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" htmlFor="price">
+                  Price
+                </label>
+                <input
+                  className="w-full p-2 border rounded-md"
+                  type="number"
+                  id="price"
+                  name="price"
+                  value={product.price}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-bold mb-2"
+                  htmlFor="description"
+                >
+                  Description
+                </label>
+                <textarea
+                  className="w-full p-2 border rounded-md"
+                  id="description"
+                  name="description"
+                  value={product.description}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-bold mb-2"
+                  htmlFor="imageUrl"
+                >
+                  Image
+                </label>
+                <input
+                  type="file"
+                  id="imageUrl"
+                  name="imageUrl"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                />
+                {product.imageUrl && (
+                  <img
+                    src={product.imageUrl}
+                    alt="Product Preview"
+                    className="mt-2 w-32 h-32 object-cover rounded"
+                  />
+                )}
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="text-white !bg-blue-5 hover:!bg-blue-7 font-bold py-2 px-4 rounded"
+                >
+                  Add Product
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </Helmet>
+  );
+};
+
+export default AddProducts;
